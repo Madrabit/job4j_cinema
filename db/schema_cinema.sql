602f2f3401b20d630b4855010fe00d590d24a470
madrabit <madrabot@gmail.com>
2020-06-11 11:17:54 +0300

    Сервис - Кинотеатр[#276719] добавил констрейнты на таблицы

--- db/schema_cinema.sql
+++ db/schema_cinema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS halls;
 DROP TABLE IF EXISTS accounts;
-CREATE TABLE accounts (id SERIAL PRIMARY KEY, name TEXT, phone INTEGER);
-CREATE TABLE halls (id SERIAL PRIMARY KEY, row INTEGER , place INTEGER, price INTEGER, busy bool, accounts_id INTEGER);
+CREATE TABLE accounts (id SERIAL PRIMARY KEY, name TEXT NOT NULL, phone INTEGER NOT NULL);
+CREATE TABLE halls (id SERIAL PRIMARY KEY, row INTEGER NOT NULL, place INTEGER NOT NULL, price INTEGER DEFAULT 0, busy bool DEFAULT false,
+  accounts_id INTEGER REFERENCES accounts);
 INSERT INTO halls(row, place, price, busy) VALUES (1, 1, 500, false );
 INSERT INTO halls(row, place, price, busy) VALUES (1, 2, 500, false );
 INSERT INTO halls(row, place, price, busy) VALUES (1, 3, 500, false );
